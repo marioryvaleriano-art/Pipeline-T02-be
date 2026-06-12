6683c644a0eabd90c4b5aead4b037f7ce3145230
Angel-Arenas <angel.arenas@vallegrande.edu.pe>
2026-06-12 00:12:59 -0500

    style(dto)limpiaron y eliminaron  comentarios redundantes en DetalleVentaRequest y DetalleVentaResponse

--- src/main/java/pe/edu/vallegrande/mybackend/dto/DetalleVentaRequest.java
+++ src/main/java/pe/edu/vallegrande/mybackend/dto/DetalleVentaRequest.java
@@ -1,16 +1,13 @@
 package pe.edu.vallegrande.mybackend.dto;
 
-import lombok.Data;
 import java.math.BigDecimal;
+import lombok.Data;
 
-//Request para detelle venta añadidos
 @Data
 public class DetalleVentaRequest {
 
     private Integer idProducto;
     private BigDecimal cantidad;
     private BigDecimal precioUnitario;
-
-    // Opcional — si no viene, el backend asume 0
     private BigDecimal descuento;
 }
